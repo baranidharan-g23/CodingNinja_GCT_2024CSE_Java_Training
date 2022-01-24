e9cbcf89e20124e8aa22f80293f92c9f6227d726
Baranidharan G <77617148+baranidharang@users.noreply.github.com>
2022-01-24 13:22:31 +0530

    Update 1. Average Marks.java
    PROBLEM STATEMENT: 
    Write a program to input a name(as a single character) and marks of three tests as m1, m2, and m3 of a student considering all the three marks have been given in integer format.
    Now, you need to calculate the average of the given marks and print it along with the name as mentioned in the output format section.
    All the test marks are in integers and hence calculate the average in integer as well. That is, you need to print the integer part of the average only and neglect the decimal part.

--- Lecture 2: Getting started with Java1/1. Average Marks.java
+++ Lecture 2: Getting started with Java1/1. Average Marks.java
@@ -5,7 +5,7 @@ public class Solution
 	public static void main(String[] args) //main function
   {
 	Scanner scan = new Scanner(System.in);
-	String Name = scan.next();//Scanning string
+	String Name = scan.next();//Scans string
     	char name = Name.charAt(0);//charAt function stores the nth character of a string to the assigned var
     	int mark1 = scan.nextInt();
     	int mark2 = scan.nextInt();
