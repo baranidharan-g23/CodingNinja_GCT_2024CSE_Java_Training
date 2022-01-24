97bef1cc76f332b444a348947fd0e089eb698dda
Baranidharan G <77617148+baranidharang@users.noreply.github.com>
2022-01-24 13:12:56 +0530

    Update 1. Average Marks.java

--- Lecture 2: Getting started with Java1/1. Average Marks.java
+++ Lecture 2: Getting started with Java1/1. Average Marks.java
@@ -1,1 +1,17 @@
 
+import java.util.Scanner;//importing Scanner library to use Scanner function
+public class Solution 
+{
+	public static void main(String[] args) //main function
+  {
+		Scanner scan = new Scanner(System.in);
+		String Name = scan.next();//Scanning string
+    char name = Name.charAt(0);//charAt function stores the nth character of a string to the assigned var
+    int mark1 = scan.nextInt();
+    int mark2 = scan.nextInt();
+    int mark3 = scan.nextInt();
+		System.out.println(name);//prints the selected string
+    System.out.print((n1+n2+n3)/3);//prints average of 3 marks
+	}
+
+}
