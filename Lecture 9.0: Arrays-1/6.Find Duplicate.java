d301a66133bed89bf08861b1a78734b672fcc7ea
Baranidharan G <77617148+baranidharang@users.noreply.github.com>
2022-02-19 14:23:26 +0530

    Create 6.Find Duplicate.java
    You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3 and among these, there is a single integer value that is present twice. You need to find and return that duplicate number present in the array.

---
+++ Lecture 9.0: Arrays-1/6.Find Duplicate.java
@@ -0,0 +1,25 @@
+public class Solution
+{  
+    public static int duplicateNumber(int arr[]) 
+    {
+        int n = arr.length;
+        int i , temp = 0, j, m = 0;
+        for(i =0;i<n;i++)
+        {
+            temp = 0;
+            for(j = 0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i!=j)
+                	{temp = 1;
+                	break;
+                	}
+            }
+            if(temp==1)
+            {
+                m = arr[i];
+                return m;
+            }    
+        }
+        return m;
+    }
+}
